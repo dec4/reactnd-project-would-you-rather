17b5993e12ae91211545b27bb4458ec03272670b
Danielle Curammeng <dcur2021@gmail.com>
2022-06-03 16:21:46 -0700

    chore: upgrade to react 17

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reducer from './reducers'
 import middleware from './middleware'
-import { createStore } from 'redux';
+import { createStore } from 'redux';  // TODO: try redux toolkit
 import { Provider } from 'react-redux';
 
 const store = createStore(reducer, middleware)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+const rootNode = document.getElementById('root');
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  rootNode
 );
